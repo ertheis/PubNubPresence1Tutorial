2725754383a97b8167b9b1e879e2df9ba136d579
ertheis <ertheis@Erics-MacBook-Pro-7.local>
2014-07-02 14:11:29 -0700

    Added PubNub Integration

---
+++ PubNubPresence1Tutorial/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  PubNubPresence1Tutorial
+//
+//  Created by Eric Theis on 7/2/14.
+//  Copyright (c) 2014 PubNub. All rights reserved.
+//
+
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import "PNImports.h"
+#endif
